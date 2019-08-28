e81b588d53b671c754e63fda4b6237336caf892f
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-08-28 13:53:31 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,43 +71,5 @@ resource "aws_instance" "nomad_instance" {
     nomad-node = var.instance_role
   }
 
-  user_data = << EOF
-		#!/usr/bin/env bash
-    
-    # create dir for nomad configuration
-    sudo mkdir -p /etc/nomad.d
-    sudo chmod 700 /etc/nomad.d
-
-    # download and run nomad configuration script
-    curl -o /tmp/nomad-${var.instance_role}-config.sh https://raw.githubusercontent.com/achuchulev/terraform-aws-nomad_instance/master/scripts/nomad-${var.instance_role}-config.sh
-    chmod +x /tmp/nomad-${var.instance_role}-config.sh
-    sudo /tmp/nomad-${var.instance_role}-config.sh ${var.nomad_region} ${var.dc} ${var.authoritative_region} '${var.retry_join}' ${var.secure_gossip}
-    rm -rf /tmp/*
-
-    # create dir for certificates and copy cfssl.json configuration file to increase the default certificate expiration time for nomad
-    mkdir -p ~/nomad/ssl
-    curl -o ~/nomad/ssl/cfssl.json https://github.com/achuchulev/terraform-aws-nomad_instance/blob/master/config/cfssl.json
-
-    # download CA certificates
-    curl -o ~/nomad/ssl/nomad-ca-key.pem https://raw.githubusercontent.com/achuchulev/terraform-aws-nomad_instance/master/ca_certs/nomad-ca-key.pem
-    curl -o ~/nomad/ssl/nomad-ca.csr https://raw.githubusercontent.com/achuchulev/terraform-aws-nomad_instance/master/ca_certs/nomad-ca.csr
-    curl -o ~/nomad/ssl/nomad-ca.pem https://raw.githubusercontent.com/achuchulev/terraform-aws-nomad_instance/master/ca_certs/nomad-ca.pem
-
-    # generate nomad node certificates
-    sudo echo '{}' | cfssl gencert -ca=nomad/ssl/nomad-ca.pem -ca-key=nomad/ssl/nomad-ca-key.pem -config=/tmp/cfssl.json -hostname='${var.instance_role}.${var.nomad_region}.nomad,localhost,127.0.0.1' - | cfssljson -bare nomad/ssl/${var.instance_role}
-
-    # copy nomad.service
-    sudo curl -o /etc/systemd/system/nomad.service https://raw.githubusercontent.com/achuchulev/terraform-aws-nomad_instance/master/config/nomad.service
-    sudo echo '{}' | cfssl gencert -ca=nomad/ssl/nomad-ca.pem -ca-key=nomad/ssl/nomad-ca-key.pem -profile=client - | cfssljson -bare nomad/ssl/cli
-
-    # Enable Nomad's CLI command autocomplete support. Skip if installed
-    grep "complete -C /usr/bin/nomad nomad" ~/.bashrc &>/dev/null || nomad -autocomplete-install
-
-    # enable and start nomad service
-    sudo systemctl enable nomad.service
-		sudo systemctl start nomad.service
-
-    # export the URL of the Nomad agent
-    echo 'export NOMAD_ADDR=https://${var.domain_name}.${var.zone_name}' >> ~/.profile
-	EOF
+  user_data = templatefile("${path.root}/templates/configuration.tmpl", { instance_role = var.instance_role, nomad_region = var.nomad_region, dc = var.dc, authoritative_region = var.authoritative_region, retry_join = var.retry_join, secure_gossip = var.secure_gossip, domain_name = var.domain_name, zone_name = var.zone_name })
 }
